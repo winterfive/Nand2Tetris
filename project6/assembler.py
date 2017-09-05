0ba5756d0f7de5b02f61aee4f8f57633580fe712
Lee <winterfive@users.noreply.github.com>
2017-09-05 15:02:32 -0400

    Update assembler.py

--- project6/assembler.py
+++ project6/assembler.py
@@ -1,63 +1,115 @@
-# Author: Lee Gainer
-# Date: Aug 2017
-# Course: NandtoTetris, final project
-# Function: Translate any .asm file to binary (.hack)
+# create dict symbolTable
+symbolTable = {'SP': '0', 'R0': '0', 'LCL': '1', 'R1': '1', 'ARG': '2', 'R2': '2',
+	'THIS': '3', 'R3': '3', 'THAT': '4', 'R4': '4', 'R5': '5', 'R6': '6', 'R7': '7',
+	'R8': '8', 'R9': '9', 'R10': '10', 'R11': '11', 'R12': '12', 'R13': '13',
+	'R14': '14', 'R15': '15', 'SCREEN': '16384', 'KBD': '24576'}
+	
+destTable = {'null': '000', 'M': '001', 'D': '010', 'MD': '011', 'A': '100',
+	'AM': '101', 'AD': '110', 'AMD': '111'}
+	
+compTable = {'0': '101010', '1': '111111', '-1': '111010', 'D': '001100', 'A': '110000',
+	'!D': '001101', '!A': '110001', '-D': '001111', '-A': '110011', 'D+1': '011111',
+	'A+1': '110111', 'D+1': '001110', 'A-1': '110010', 'D+A': '000010', 'D-A': '010011',
+	'A-D': '000111', 'D&A': '000000', 'D|A': '010101', 'M': '110000', '!M': '110001',
+	'-M': '110011', 'M+1': '110111', 'M-1': '110010', 'D+M': '000010', 'D-M': '010011',
+	'M-D': '000111', 'D&M': '000000', 'D|M': '010101'}
+	
+jumpTable = {'null': '000', 'JGT': '001', 'JEQ': '010', 'JGE': '011', 'JLT': '100',
+	'JNE': '101', 'JLE': '110', 'JMP': '111'}
+		
+def findValueA(value):
+	# A instruction
+	# Purpose: return 16bit binary value of the arguement
+	# Stub: string -> string
 
-#!/usr/bin/python
+	# remove @ from string
+	value = value.strip('@')
 
-import sys
-from helpers import *
+	# change value to an int
+	value = int(value)
 
-# create empty symbol table, dict
-# add pre-defined symbols
+	# find value in binary
+	value = format(value, 'b')
 
-# 1st Pass - scan program
-# 		   - add labels and their values to table
+	# if value(len) < 16:
+	value = value.zfill(16)
 
-# 2nd Pass - scan program for each instruction
-#		   - find binary value for each instruction and place in output file
+	# return value
+	return value
 
-x = ''
-
-# get name for new file
-title = sys.argv[1][0:3]
-
-# concatenate title with ".hack"
-title = title + '.hack'
-
-# create hack file to write to
-h = open(title, 'w')
-
-# open file.asm to be translated
-with open(sys.argv[1], 'r') as f:
-    for line in f:
-
-        # strip \n from each line
-        line = line.strip('\n')
-        
-        # if line is a comment
-        if '//' in line:
-            x = 'comment'
-            continue
-
-        # if line is empty
-        if not line:
-            x = 'empty'
-            continue
-
-        # if line is a Register address (label)
-        # if line[0] is '@' and line[1].isalpha():
-        # 	pass
-
-        # if line is an A instruction
-        if line[0] == '@' and line[1].isdigit():
-            x = findValueA(line)
-
-        # if line is a C instruction
-        if line[0].isalpha() and line[1] == '=':
-            x = findValueC(line)
-
-        # write x to hack file
-        h.write(x + '\n')
-
-h.close()
+def findValueC(value):
+	# C Instruction or Jump
+	# Purpose: return 16bit binary value of the arguement
+	# Stub: string -> string
+	
+	# if value is a jump
+	if ';' in value:
+		
+		# if jump contains dest, comp, and jump
+		if '=' in value:
+			# parse jump data
+			value = value.split(';')
+			compAndDest = value[0]
+			jump = value[1]
+			
+			# parse comp and dest
+			compAndDest = compAndDest.split('=')
+			dest = compAndDest[0]
+			comp = compAndDest[1]
+			
+			# find value of a
+			a = findAbitValue(comp)
+			
+			# find values for comp, jump, dest
+			dest = destTable[dest]
+			comp = compTable[comp]
+			jump = jumpTable[jump]
+			
+		# jump contains only comp and jump
+		else:
+			# parse the comp and dest data
+			value = value.split(';')
+			comp = value[0]
+			jump = value[1]
+			
+			# find value of a
+			a = findAbitValue(comp)
+			
+			# find values for comp, jump, dest
+			comp = compTable[comp]
+			jump = jumpTable[jump]
+			dest = destTable['null']
+		
+	# value isn't a jump
+	else:
+		# parse the dest and comp data
+		value = value.split('=')
+		dest = value[0]
+		comp = value[1]
+		
+		# find value of a
+		a = findAValue(comp)
+		
+		# find values for each
+		comp = compTable[comp]
+		dest = destTable[dest]
+		jump = jumpTable['null']
+		
+	# concatenate parts and store value
+	value = ('111' + a + comp + dest + jump)
+	
+	# return value
+	return value
+	
+def findAbitValue(comp):
+	# A Value in a C Instruction
+	# Purpose: Find 1bit value for a
+	#Stub: string -> string
+	
+	# find value for a
+	if 'M' in comp:
+		a = '1'
+	else:
+		a = '0'
+		
+	return  a
