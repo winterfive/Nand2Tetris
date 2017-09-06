bdc262460853c15900813f99c223682fb37fb4a7
Lee <winterfive@users.noreply.github.com>
2017-09-06 12:05:40 -0400

    Changes to begin handling labels

--- project6/helpers.py
+++ project6/helpers.py
@@ -9,7 +9,7 @@ destTable = {'null': '000', 'M': '001', 'D': '010', 'MD': '011', 'A': '100',
 	
 compTable = {'0': '101010', '1': '111111', '-1': '111010', 'D': '001100', 'A': '110000',
 	'!D': '001101', '!A': '110001', '-D': '001111', '-A': '110011', 'D+1': '011111',
-	'A+1': '110111', 'D+1': '001110', 'A-1': '110010', 'D+A': '000010', 'D-A': '010011',
+	'A+1': '110111', 'D-1': '001110', 'A-1': '110010', 'D+A': '000010', 'D-A': '010011',
 	'A-D': '000111', 'D&A': '000000', 'D|A': '010101', 'M': '110000', '!M': '110001',
 	'-M': '110011', 'M+1': '110111', 'M-1': '110010', 'D+M': '000010', 'D-M': '010011',
 	'M-D': '000111', 'D&M': '000000', 'D|M': '010101'}
@@ -24,6 +24,12 @@ def findValueA(value):
 
 	# remove @ from string
 	value = value.strip('@')
+	
+	# HANDLE LABELS HERE  TODO
+	# check table for remaining value
+	# if in table, return value
+	# else, place new key and value into table
+	# replace label with value
 
 	# change value to an int
 	value = int(value)
@@ -38,7 +44,7 @@ def findValueA(value):
 	return value
 
 def findValueC(value):
-	# C Instruction w/wo jump
+	# C Instruction or Jump
 	# Purpose: return 16bit binary value of the arguement
 	# Stub: string -> string
 	
