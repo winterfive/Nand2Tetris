4f74d36432047e3f7b5c6b19b3bfef2d0b26f5e7
Lee <winterfive@users.noreply.github.com>
2017-09-06 13:43:11 -0400

    Update helpers.py

--- project6/helpers.py
+++ project6/helpers.py
@@ -1,70 +1,153 @@
-# Author: Lee Gainer
-# Date: Aug 2017
-# Course: NandtoTetris, final project
-# Function: Translate any .asm file to binary (.hack)
+# create dict symbolTable
+symbolTable = {'SP': '0', 'R0': '0', 'LCL': '1', 'R1': '1', 'ARG': '2', 'R2': '2',
+	'THIS': '3', 'R3': '3', 'THAT': '4', 'R4': '4', 'R5': '5', 'R6': '6', 'R7': '7',
+	'R8': '8', 'R9': '9', 'R10': '10', 'R11': '11', 'R12': '12', 'R13': '13',
+	'R14': '14', 'R15': '15', 'SCREEN': '16384', 'KBD': '24576'}
+	
+destTable = {'null': '000', 'M': '001', 'D': '010', 'MD': '011', 'A': '100',
+	'AM': '101', 'AD': '110', 'AMD': '111'}
+	
+compTable = {'0': '101010', '1': '111111', '-1': '111010', 'D': '001100', 'A': '110000',
+	'!D': '001101', '!A': '110001', '-D': '001111', '-A': '110011', 'D+1': '011111',
+	'A+1': '110111', 'D-1': '001110', 'A-1': '110010', 'D+A': '000010', 'D-A': '010011',
+	'A-D': '000111', 'D&A': '000000', 'D|A': '010101', 'M': '110000', '!M': '110001',
+	'-M': '110011', 'M+1': '110111', 'M-1': '110010', 'D+M': '000010', 'D-M': '010011',
+	'M-D': '000111', 'D&M': '000000', 'D|M': '010101'}
+	
+jumpTable = {'null': '000', 'JGT': '001', 'JEQ': '010', 'JGE': '011', 'JLT': '100',
+	'JNE': '101', 'JLE': '110', 'JMP': '111'}
+	
+def prepLine(line):
+	# Prepare all lines for analysis
+	# Purpose: Remove whitespace and comments from all lines
+	# Stub: string -> string
 
-#!/usr/bin/python
+	# strip \n from each line
+    line = line.strip('\n')
 
-import sys
-from helpers import *
+    # remove in-line comments
+    if '/' in line:
+	    line = line.split('/')
+	    line = line[0]
 
-# create empty symbol table, dict
-# add pre-defined symbols
+	line = line.strip()
 
-# 1st Pass - scan program
-# 		   - add labels and their values to table
+    return line
 
-# 2nd Pass - scan program for each instruction
-#		   - find binary value for each instruction and place in output file
+def handleLabelDeclarations(value, n):
+	# Label Declaration
+	# Purpose: Place label name & value in table
+	# Stub: string, int -> null
 
-x = ''
-n = 0
+	# remove () from value
+	value = value.strip('()')
 
-# get name for new file
-title = sys.argv[1]
+	# create variable to hold value keyname
+	keyName = value
 
-# split name from file type
-title = title.split('.')
-title = title[0]
+	# replace label with n (line count via arguement)
+	value = n + 1
 
-# concatenate title with ".hack"
-title = title + '.hack'
+	# place new symbol in table
+	symbolTable[keyName] = value
 
-# create hack file to write to
-h = open(title, 'w')
+def findValueA(value, n):
+	# A instruction
+	# Purpose: return 16bit binary value of the arguement
+	# Stub: string, int -> string
 
-# open file.asm to be translated
-with open(sys.argv[1], 'r') as f:
-    
-    # first loop: find labels, store labels in table
-    for line in f:
-        
-        if '()' in line:
-            x = handleLabelDec(line, n)
-            
-        n += 1
-        
-    # reset n
-    n = 0
-            
-    # evaluate all lines, find binary values for each
-    for line in f:
-        
-        # prep line for analysis
-        line = prepLine(line)
-            
-        # if line is an A instruction
-        if '@' in line:
-            x = findValueA(line, n)
+	# remove @ from string
+	value = value.strip('@')
 
-        # if line is a C instruction
-        # if line[0].isalpha() and line[1] == '=':
-        else:
-            x = findValueC(line)
+	# if symbol in table, return value
+	if value in symbolTable:
+		value = symbolTable[value]
 
-        # write x to hack file
-        h.write(x + '\n')
-        
-        n += 1
+	# change value to an int
+	value = int(value)
 
-h.close()
+	# find value in binary
+	value = format(value, 'b')
+
+	# if value(len) < 16:
+	value = value.zfill(16)
+
+	# return value
+	return value
+
+def findValueC(value):
+	# C Instruction or Jump
+	# Purpose: return 16bit binary value of the arguement
+	# Stub: string -> string
+
+	# if value is a jump
+	if ';' in value:
+
+		# if jump contains dest, comp, and jump
+		if '=' in value:
+			# parse jump data
+			value = value.split(';')
+			compAndDest = value[0]
+			jump = value[1]
+
+			# parse comp and dest
+			compAndDest = compAndDest.split('=')
+			dest = compAndDest[0]
+			comp = compAndDest[1]
+
+			# find value of a
+			a = findAbitValue(comp)
+
+			# find values for comp, jump, dest
+			dest = destTable[dest]
+			comp = compTable[comp]
+			jump = jumpTable[jump]
+			
+		# jump contains only comp and jump
+		else:
+			# parse the comp and dest data
+			value = value.split(';')
+			comp = value[0]
+			jump = value[1]
+			
+			# find value of a
+			a = findAbitValue(comp)
+			
+			# find values for comp, jump, dest
+			comp = compTable[comp]
+			jump = jumpTable[jump]
+			dest = destTable['null']
+		
+	# value isn't a jump
+	else:
+		# parse the dest and comp data
+		value = value.split('=')
+		dest = value[0]
+		comp = value[1]
+		
+		# find value of a
+		a = findAbitValue(comp)
+		
+		# find values for each
+		comp = compTable[comp]
+		dest = destTable[dest]
+		jump = jumpTable['null']
+		
+	# concatenate parts and store value
+	value = ('111' + a + comp + dest + jump)
+	
+	# return value
+	return value
+	
+def findAbitValue(comp):
+	# A Value in a C Instruction
+	# Purpose: Find 1bit value for a
+	#Stub: string -> string
+	
+	# find value for a
+	if 'M' in comp:
+		a = '1'
+	else:
+		a = '0'
+		
+	return  a
